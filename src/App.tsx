23a44d22c067fb95b2ddcc5a5afdd8ee175f8cc4
Potapov Vladislav <89172111013q@gmail.com>
2024-02-26 08:43:01 +0300

    create project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import { Calendar } from "./components/Calendar/Calednar"
+import { CurrentDay } from "./components/CurrentDay/CurrentDay"
+import { Header } from "./components/Header/Header"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <CurrentDay />
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
